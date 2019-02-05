93166dacd613894593d607254f91cdf379630f25
Zeeshan Rasool <zrasool@mbrhe.ae>
2019-02-05 12:35:12 +0400

    Added Notification Badges in Android Code

--- app/src/main/java/com/zeeshan/rasool/MainActivity.java
+++ app/src/main/java/com/zeeshan/rasool/MainActivity.java
@@ -1,6 +1,9 @@
 package com.zeeshan.rasool;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.widget.Toast;
@@ -23,8 +26,20 @@ public class MainActivity extends AppCompatActivity {
                 if (!TextUtils.isEmpty(name)) {
                     Toast.makeText(MainActivity.this, "From String Extra" + name, Toast.LENGTH_SHORT).show();
                 }
+            } else if (getIntent().hasExtra("showNotification")) {
+                final int showNotification = getIntent().getIntExtra("showNotification", -1);
+                if (showNotification != -1) {
+                    final NotificationHelper notificationHelper = new NotificationHelper(MainActivity.this);
+                    final Handler handler = new Handler();
+                    final Runnable r = new Runnable() {
+                        @RequiresApi(api = Build.VERSION_CODES.O)
+                        public void run() {
+                            notificationHelper.createNotification("ZEESHAN", "From Int Extra" + showNotification);
+                        }
+                    };
+                    handler.postDelayed(r, 1500);
+                }
             }
         }
-
     }
 }
